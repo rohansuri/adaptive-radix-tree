5cdc79eb5054524a32925f889d53334ae0762c73
Rohan Suri <rohan.a.suri@gmail.com>
2019-05-26 21:51:52 +0530

    Add prefix string test (explicitly null terminated for now)

--- src/test/java/art/ARTTest.java
+++ src/test/java/art/ARTTest.java
@@ -51,4 +51,30 @@ public class ARTTest {
 		Assert.assertEquals("1", art.put(BAR, "2"));
 		Assert.assertEquals("2", art.get(BAR));
 	}
+
+	// we'll have to provide nice Serdes that can take care of strings this way
+	@Test
+	public void testPrefixesInsert(){
+		ART<String> art = new ART<>();
+
+		byte[] bar = nullTerminated(BAR);
+		Assert.assertNull(art.put(bar, "1"));
+		Assert.assertEquals("1", art.get(bar));
+		Assert.assertNull(art.get(BAR));
+
+		byte[] barca = nullTerminated("BARCA".getBytes());
+		Assert.assertNull(art.put(barca, "2"));
+		Assert.assertEquals("2", art.get(barca));
+	}
+
+	private byte[] nullTerminated(byte[] key){
+		// is this the best way?
+		byte[] keyBytes = new byte[key.length + 1];
+		System.arraycopy(key, 0, keyBytes, 0, key.length);
+		return keyBytes;
+	}
+
+	private byte[] nullTerminated(String key){
+		return nullTerminated(key.getBytes());
+	}
 }
