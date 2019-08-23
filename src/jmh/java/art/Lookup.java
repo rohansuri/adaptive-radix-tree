c658ab3bb8331e9b248cee082e294b2dcb508997
Rohan Suri <rohan.a.suri@gmail.com>
2019-08-23 10:04:39 +0800

    add lookup dense benchmark and save results

---
+++ src/jmh/java/art/Lookup.java
@@ -0,0 +1,72 @@
+package art;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Supplier;
+import java.util.NavigableMap;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+// https://github.com/openjdk/jdk13/blob/master/test/micro/org/openjdk/bench/java/util/HashMapBench.java
+public class Lookup {
+
+	@State(Scope.Benchmark)
+	public static class Dense {
+		Map<Integer, Object> m;
+		@Param("65000")
+		int size;
+
+		public enum MapType {
+			HASH_MAP,
+			ART,
+			TREE_MAP
+		}
+
+		@Param
+		MapType mapType;
+
+		@Setup
+		public void setup() {
+			switch (mapType) {
+			case HASH_MAP:
+				m= new HashMap<>();
+				break;
+			case ART:
+				m =  new AdaptiveRadixTree<>(BinaryComparable.INTEGER);
+				break;
+			case TREE_MAP:
+				m = new TreeMap<>();
+				break;
+			default:
+				throw new AssertionError();
+			}
+			Object holder = new Object();
+			List<Integer> keys = new ArrayList<>(size);
+			// dense keys
+			for(int i = 0; i < size; i++){
+				keys.add(i);
+			}
+			// randomly permute keys
+			Collections.shuffle(keys);
+
+			for(int key: keys){
+				m.put(key, holder);
+			}
+		}
+	}
+
+	@Benchmark
+	@BenchmarkMode(Mode.Throughput)
+	public Object dense(Dense d){
+		int x = ThreadLocalRandom.current().nextInt(0, d.size);
+		return d.m.get(x);
+	}
+}
