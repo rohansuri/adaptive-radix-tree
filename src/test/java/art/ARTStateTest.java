618bc82407ed13cb1f9e35731a8b58701524f3e0
Rohan Suri <rohan.a.suri@gmail.com>
2019-09-06 00:18:35 +0800

    Mark ARTStateTest as todo

--- src/test/java/art/ARTStateTest.java
+++ src/test/java/art/ARTStateTest.java
@@ -1,5 +1,11 @@
 package art;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 // contains tests to assert internal structure of ART as expected when making the ART on paper
 public class ARTStateTest {
+	@Test
+	@Ignore
+	public void todo(){}
 }
