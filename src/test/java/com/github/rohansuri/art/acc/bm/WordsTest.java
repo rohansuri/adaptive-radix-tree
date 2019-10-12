8702985e52e3335af7613ce27f2baeb0322aaf5a
Rohan Suri <rohan.a.suri@gmail.com>
2019-10-12 23:26:19 +0800

    Add WordsTest that runs short test suite for 200k+ english words (taken from plar)

---
+++ src/test/java/com/github/rohansuri/art/acc/bm/WordsTest.java
@@ -0,0 +1,86 @@
+package com.github.rohansuri.art.acc.bm;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import java.util.UUID;
+
+import com.github.rohansuri.art.AbstractNavigableMapShortTest;
+import com.github.rohansuri.art.AdaptiveRadixTree;
+import com.github.rohansuri.art.BinaryComparables;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class WordsTest extends AbstractNavigableMapShortTest<String, String> {
+	private static final String[] sampleKeys = loadWords();
+	private static final String[] newSampleValues = reverseSampleKeys();
+	private static final String[] otherKeys = generateOtherKeys();
+
+	private static String[] generateOtherKeys() {
+		int n = 10;
+		String[] otherKeys = new String[n];
+		for (int i = 0; i < 10; i++) {
+			otherKeys[i] = UUID.randomUUID().toString();
+		}
+		return otherKeys;
+	}
+
+	private static String[] reverseSampleKeys() {
+		String[] keys = sampleKeys.clone();
+		ArrayUtils.reverse(keys);
+		return keys;
+	}
+
+	private static String[] loadWords() {
+		try {
+			List<String> lines = IOUtils
+					.readLines(WordsTest.class.getResourceAsStream("/words.txt"), StandardCharsets.UTF_8);
+			return lines.toArray(new String[0]);
+		}
+		catch (IOException e) {
+			throw new RuntimeException("failed to load words", e);
+		}
+	}
+
+	public WordsTest(String testName) {
+		super(testName);
+	}
+
+	@Override
+	public String[] getSampleKeys() {
+		return sampleKeys;
+	}
+
+	@Override
+	public String[] getSampleValues() {
+		return sampleKeys;
+	}
+
+	@Override
+	public String[] getNewSampleValues() {
+		return newSampleValues;
+	}
+
+	@Override
+	public String[] getOtherKeys() {
+		return otherKeys;
+	}
+
+	@Override
+	public String[] getOtherValues() {
+		return otherKeys;
+	}
+
+	@Override
+	public NavigableMap<String, String> makeObject() {
+		return new AdaptiveRadixTree<>(BinaryComparables.forUTF8());
+	}
+}
