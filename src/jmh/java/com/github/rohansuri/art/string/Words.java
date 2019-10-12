67d24eee52599163a5e30a374b81c0defb713f4c
Rohan Suri <rohan.a.suri@gmail.com>
2019-10-13 00:08:46 +0800

    Add Words benchmark (words.txt borrowed from plar)

---
+++ src/jmh/java/com/github/rohansuri/art/string/Words.java
@@ -0,0 +1,85 @@
+package com.github.rohansuri.art.string;
+
+import com.github.rohansuri.art.AdaptiveRadixTree;
+import com.github.rohansuri.art.BinaryComparables;
+
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.*;
+import org.apache.commons.collections4.trie.*;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Supplier;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+public class Words {
+	private static final int WORDS = 235886;
+
+	@State(Scope.Benchmark)
+	public static class Data {
+		Object holder;
+		String[] keys;
+		Supplier<Map<String, Object>> supplier;
+
+		public enum MapType {
+			HASH_MAP,
+			ART,
+			TREE_MAP,
+			PATRICIA_TRIE
+		}
+
+		@Param
+		MapType mapType;
+
+		@Setup
+		public void setup() throws IOException, URISyntaxException {
+			switch (mapType) {
+			case HASH_MAP:
+				supplier = () -> new HashMap<>();
+				break;
+			case ART:
+				supplier = () -> new AdaptiveRadixTree<>(BinaryComparables.forUTF8());
+				break;
+			case TREE_MAP:
+				supplier = () -> new TreeMap<>();
+				break;
+			case PATRICIA_TRIE:
+				supplier = () -> new PatriciaTrie<Object>();
+				break;
+			default:
+				throw new AssertionError();
+			}
+
+			holder = new Object();
+			List<String> s = IOUtils
+					.readLines(this.getClass().getResourceAsStream("/words.txt"), StandardCharsets.UTF_8);
+			keys = s.toArray(String[]::new);
+			if (keys.length != WORDS) {
+				throw new AssertionError("expected " + WORDS + " words from the file, got " + keys.length);
+			}
+		}
+	}
+
+	@Benchmark
+	@BenchmarkMode({Mode.AverageTime})
+	@OutputTimeUnit(TimeUnit.NANOSECONDS)
+	public int insert(Blackhole bh, Data d) {
+		Map<String, Object> m = d.supplier.get();
+		for (int i = 0; i < d.keys.length; i++) {
+			bh.consume(m.put(d.keys[i], d.holder));
+		}
+		return m.size();
+	}
+}
