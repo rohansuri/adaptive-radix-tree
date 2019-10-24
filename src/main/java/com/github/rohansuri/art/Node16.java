943bd7820e171ba716d4fcf0eac63917e0a4c3f1
Rohan Suri <rohan.a.suri@gmail.com>
2019-10-24 09:53:52 +0800

    loop over for Node16.findChild rather than binary searching

--- src/main/java/com/github/rohansuri/art/Node16.java
+++ src/main/java/com/github/rohansuri/art/Node16.java
@@ -43,14 +43,12 @@ class Node16 extends InnerNode {
 	public Node findChild(byte partialKey) {
 		// TODO: use simple loop to see if -XX:+SuperWord applies SIMD JVM instrinsics
 		partialKey = BinaryComparableUtils.unsigned(partialKey);
-		// binary search for key
-		// having the from and to gives us only a valid view into what are the
-		// valid array elements that actually have keys and are not ABSENT
-		int index = Arrays.binarySearch(keys, 0, noOfChildren, partialKey);
-		if (index < 0) {
-			return null;
+		for(int i = 0; i < noOfChildren; i++){
+			if(keys[i] == partialKey){
+				return child[i];
+			}
 		}
-		return child[index];
+		return null;
 	}
 
 	@Override
