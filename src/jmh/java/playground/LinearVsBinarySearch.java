2073509ba4ad4f10c3dea448c2890d3bdf7d5e7f
Rohan Suri <rohan.a.suri@gmail.com>
2019-11-06 22:29:22 +0800

    Add linear search unrolled benchmark
    Unrolling for 16 size array is about 28% faster than looped linear search.
    Even though unrolled version has more branches, it has fewer branch misses.
    I guess that makes the difference? (verify)
    
    TODO:
    1) investigate assembly to understand why unrolling is faster.
    2) why JVM/JIT does not unroll the loop for us.

--- src/jmh/java/playground/LinearVsBinarySearch.java
+++ src/jmh/java/playground/LinearVsBinarySearch.java
@@ -3,6 +3,7 @@ package playground;
 import com.github.rohansuri.art.string.LargeData;
 import org.apache.commons.lang3.ArrayUtils;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.Arrays;
@@ -12,50 +13,50 @@ import java.util.concurrent.TimeUnit;
 
 public class LinearVsBinarySearch {
 
-    @State(Scope.Benchmark)
-    public static class Data {
-        byte keys[];
-        byte toLookup[];
+	@State(Scope.Benchmark)
+	public static class Data {
+		byte keys[];
+		byte toLookup[];
 
-        @Param({"4", "16"})
-        int size;
+		@Param({"4", "16"})
+		int size;
 
-        @Setup
-        public void setup() {
-            keys = new byte[size];
-            toLookup = new byte[size];
-            ThreadLocalRandom.current().nextBytes(keys);
-            Arrays.sort(keys);
-            System.arraycopy(keys, 0, toLookup, 0, keys.length);
-            ArrayUtils.shuffle(toLookup);
-        }
-    }
+		@Setup
+		public void setup() {
+			keys = new byte[size];
+			toLookup = new byte[size];
+			ThreadLocalRandom.current().nextBytes(keys);
+			Arrays.sort(keys);
+			System.arraycopy(keys, 0, toLookup, 0, keys.length);
+			ArrayUtils.shuffle(toLookup);
+		}
+	}
 
-    @Benchmark
-    @BenchmarkMode({Mode.AverageTime})
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public void linear(Blackhole b, Data d) {
-        for (int i = 0; i < d.toLookup.length; i++) {
-            b.consume(linear(d.keys, d.toLookup[i]));
-        }
-    }
+	@Benchmark
+	@BenchmarkMode({Mode.AverageTime})
+	@OutputTimeUnit(TimeUnit.NANOSECONDS)
+	public void linear(Blackhole b, Data d) {
+		for (int i = 0; i < d.toLookup.length; i++) {
+			b.consume(linear(d.keys, d.toLookup[i]));
+		}
+	}
 
-    private int linear(byte[] keys, byte key) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == key) {
-                return i;
-            }
-        }
-        return -1;
-    }
+	private int linear(byte[] keys, byte key) {
+		for (int i = 0; i < keys.length; i++) {
+			if (keys[i] == key) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
-    @Benchmark
-    @BenchmarkMode({Mode.AverageTime})
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public void binary(Blackhole b, Data d) {
-        for (int i = 0; i < d.toLookup.length; i++) {
-            b.consume(Arrays.binarySearch(d.keys, d.toLookup[i]));
-        }
-    }
+	@Benchmark
+	@BenchmarkMode({Mode.AverageTime})
+	@OutputTimeUnit(TimeUnit.NANOSECONDS)
+	public void binary(Blackhole b, Data d) {
+		for (int i = 0; i < d.toLookup.length; i++) {
+			b.consume(Arrays.binarySearch(d.keys, d.toLookup[i]));
+		}
+	}
 
 }
