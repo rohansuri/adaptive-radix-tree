0cf1ff93ad06da4396477b1b5d2ac1da24ef6c87
Rohan Suri <rohan.a.suri@gmail.com>
2019-11-07 01:18:03 +0800

    Remove BinaryComparable string terminator unit test since we allow prefix keys now

--- src/test/java/com/github/rohansuri/art/BinaryComparableUtilsTest.java
+++ src/test/java/com/github/rohansuri/art/BinaryComparableUtilsTest.java
@@ -9,46 +9,6 @@ import org.junit.jupiter.api.Test;
 
 public class BinaryComparableUtilsTest {
 
-	@Test
-	public void testTerminator() {
-		// empty string case
-		assertArray("");
-
-		// normal case, test last portion copy
-		assertArray("barca");
-	}
-
-	@Test
-	public void testTerminatorNullCase() {
-		String s = new String(new byte[] {0}); // null
-		byte[] bytes = BinaryComparables.forUTF8().get(s);
-		int terminatorLen = BinaryComparableUtils.TERMINATOR.length;
-		assertEquals(s.length() + terminatorLen + 1, bytes.length);
-		byte[] expected = new byte[] {0, 1, 0, 0};
-		assertArrayEquals(expected, bytes);
-	}
-
-	@Test
-	public void testTerminatorMultipleNullsCase() {
-		String s = new String(new byte[] {0, 76, 0, 69, 0, 79, 0}); // null, L, null, E, null, O, null
-		byte[] bytes = BinaryComparables.forUTF8().get(s);
-		int terminatorLen = BinaryComparableUtils.TERMINATOR.length;
-		// 3 because of the three "1" bytes added for each of the nulls
-		assertEquals(s.length() + terminatorLen + 4, bytes.length);
-		byte[] expected = new byte[] {0, 1, 76, 0, 1, 69, 0, 1, 79, 0, 1, 0, 0};
-		assertArrayEquals(expected, bytes);
-	}
-
-	private void assertArray(String s) {
-		int terminatorLen = BinaryComparableUtils.TERMINATOR.length;
-		byte[] bytes = BinaryComparables.forUTF8().get(s);
-		assertEquals(s.length() + terminatorLen, bytes.length);
-		ByteArrayOutputStream expected = new ByteArrayOutputStream(s.length() + terminatorLen);
-		expected.write(s.getBytes(StandardCharsets.US_ASCII), 0, s.length());
-		expected.write(BinaryComparableUtils.TERMINATOR, 0, terminatorLen);
-		assertArrayEquals(expected.toByteArray(), bytes);
-	}
-
 	/*
 		-128 to 127 available signed range
 		but we want to interpret it as unsigned (since Java does not have unsigned types)
