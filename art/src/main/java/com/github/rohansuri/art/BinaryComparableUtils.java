29a134f57d3fc294f0cb54bed4214871d8f5d2f8
rohan.suri <rohan.suri@axinan.com>
2019-12-16 21:10:44 +0800

    remove BinaryComparableUtil to 00 terminate strings

--- art/src/main/java/com/github/rohansuri/art/BinaryComparableUtils.java
+++ art/src/main/java/com/github/rohansuri/art/BinaryComparableUtils.java
@@ -9,39 +9,6 @@ class BinaryComparableUtils {
 		throw new AssertionError();
 	}
 
-	private static int countZeroBytes(byte[] key) {
-		int count = 0;
-		for (byte b : key) {
-			count += b == 0 ? 1 : 0;
-		}
-		return count;
-	}
-
-	// in UTF-8 encoding, other than the 0 byte from the ASCII subset
-	// no codepoint ever will have a 0 byte
-	// because of the continuation bytes which always have a 1 MSB.
-	static byte[] terminate(byte[] key) {
-		int count = countZeroBytes(key);
-		int end = key.length + count; // to add ZERO byte 1
-		byte[] terminatedKey = new byte[end + 2];
-		if (count == 0) {
-			System.arraycopy(key, 0, terminatedKey, 0, key.length);
-		}
-		else {
-			for (int i = 0, j = 0; i < key.length; i++, j++) {
-				terminatedKey[j] = key[i];
-				if (key[i] == 0) {
-					terminatedKey[++j] = 1;
-				}
-			}
-		}
-		// terminator should be the smallest allowed byte value to obey binary comparability
-		// of variable length keys
-		terminatedKey[end] = 0;
-		terminatedKey[end + 1] = 0;
-		return terminatedKey;
-	}
-
 	// 2^7 = 128
 	private static final int BYTE_SHIFT = 1 << Byte.SIZE - 1;
 
